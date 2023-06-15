983f65ae9da7ec85c26d18ebac27868f809c48b6
YAbounouar <yness445@gmail.com>
2023-06-15 16:55:57 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char *message = "Programming is like building a multilingual puzzle\n";
-	
-	puts(message);
-	return (0);
+    char *message = "\"Programming is like building a multilingual puzzle";
+
+    puts(message);
+    return (0);
 }
