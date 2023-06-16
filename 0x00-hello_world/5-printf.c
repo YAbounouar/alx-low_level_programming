545ef387396e3fdf4415d009018ccb8ed9e674a8
YAbounouar <yness445@gmail.com>
2023-06-15 17:23:08 -0700

    555

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
 
 /**
-
-* main - Entry point
-
-*
-
-* Return: Always 0 (Success)
-
-*/
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 
 int main(void)
 
