16aaaa603b72bca3532438c597e7a626ec580c24
YAbounouar <yness445@gmail.com>
2023-06-15 17:59:41 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -16,13 +16,8 @@ int fd;
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t len = 0;
 
-fd = open("/dev/stderr", O_WRONLY);
-if (fd == -1)
-return (1);
-
-while (message[len] != '\0')
-len++;
-
-write(2,  “the text\n” ,  59);
+	write(fd, message, len);
+	close(fd); 
 
 return (1);
+}
