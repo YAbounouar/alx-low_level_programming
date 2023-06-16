3bd08afd0dc78a893f5390d08606d9794bf466b0
YAbounouar <yness445@gmail.com>
2023-06-15 18:12:33 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point of the program
  *
@@ -8,6 +9,6 @@
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, message, 59); 
+	write(1, message, 59); 
 return (1);
 }
