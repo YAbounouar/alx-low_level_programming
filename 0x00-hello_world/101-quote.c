7b3dea22d1c59caca8b76644c4ddce3bf62cd592
YAbounouar <yness445@gmail.com>
2023-06-15 18:05:35 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,19 +3,15 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
 /**
  * main - Entry point of the program
  *
  * Return: Always 1 (indicating successful execution)
  */
-
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t len = 59;
-
-	write(1, message, len); 
-
+write(1, message, len); 
 return (1);
 }
