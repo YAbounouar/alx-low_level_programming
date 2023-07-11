639c998b8416976ac09f9d865e214a6e394d6634
YAbounouar <132454374+YAbounouar@users.noreply.github.com>
2023-07-11 16:41:50 +0100

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-  * _strdup - Duplicate a string
-  * @str: the string to duplicate
-  *
-  * Return: the string duplicated
-  */
+ * _strdup - Duplicate a string
+ * @str: the string to duplicate
+ *
+ * Return: the string duplicated
+ */
 char *_strdup(char *str)
 {
 	int a = 0, i = 1;
